c1fa759526c761849e9f2f40d576cf9d6a36e6cc
Larry Zhao <245313315@qq.com>
2017-11-20 08:52:33 +0000

    Sync with the latest local

--- Program.cs
+++ Program.cs
@@ -1,105 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
+using System.Linq;
 
-namespace Student_Record
+namespace StudentRecords
 {
 	class Program
 	{
+		static List<string> records = new List<string>();
+
 		static void Main(string[] args)
 		{
-			string[] studentRecords = new string[0];
-			bool exitProgram = false;
-			Console.WriteLine("Welcome to Student Records\n");
-			Console.WriteLine("Menu");
-			Console.WriteLine("1. Read File");
-			Console.WriteLine("2. Output 1 Student Record");
-			Console.WriteLine("3. Output all Student Record");
-			Console.WriteLine("Enter E to exit");
-			while (exitProgram == false)
+			Console.WriteLine("Welcome to Student Records");
+			ReadInFile();
+			bool keepGoing = true;
+			while (keepGoing)
 			{
-				Console.WriteLine("\nEnter your option:");
-				string opt = Console.ReadLine().ToLower();
-				if (opt == "1")
+				Console.WriteLine("1 - ");
+				Console.WriteLine("2 - Find Student by index No.");
+				Console.WriteLine("3 - Find First Match");
+				Console.WriteLine("4 - Output All Records");
+				Console.WriteLine("5 - Find Student by Student No.");
+				Console.WriteLine("9 - Quit");
+				Console.Write("Select an option: ");
+				char menuChoice = Console.ReadLine().ToCharArray()[0];
+				Console.WriteLine();
+				switch (menuChoice)
 				{
-					studentRecords = readFile();
-				}
-				else if (opt == "2")
-				{
-					outputSingle(studentRecords);
-				}
-				else if (opt == "3")
-				{
-					outputAll(studentRecords);
-				}
-				else if (opt == "e")
-				{
-					exitProgram = true;
-				}
-				else
-				{
-					Console.WriteLine("Invalid Input");
+					case '2':
+						FindRecordByIndex();
+						break;
+					case '3':
+						FindFirstMatch();
+						break;
+					case '4':
+						ListedAllRecords();
+						break;
+					case '5':
+						FindRecordByStudentNumber();
+						break;
+					case '9':
+						keepGoing = false;
+						break;
+					default:
+						Console.WriteLine("Invalid menu choice");
+						break;
 				}
 			}
-			Console.ReadLine();
 		}
 
-		static private string[] readFile()
+		static void ReadInFile()
 		{
-			string[] studentRecordLocal = new string[100];
-			bool fileFound = false;
-			while (fileFound == false)
+			Console.Write("Enter FileName: ");
+			string fileName = Console.ReadLine() + ".csv";
+			try
 			{
-				Console.WriteLine("\nPlease enter the file name with extension:");
-				string fileName = Console.ReadLine();
-				try
-				{
-					using (StreamReader reader = new StreamReader(fileName))
-					{
-						int i = 0;
-						while (!reader.EndOfStream)
-						{
-							studentRecordLocal[i] = reader.ReadLine();
-							i++;
-						}
-					}
-					fileFound = true;
-				}
-				catch (FileNotFoundException)
-				{
-					Console.WriteLine("\n\tFile not found");
-				}
+				ReadFileIntoRecordsArray(fileName);
 			}
-			return studentRecordLocal;
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine("File does not exist");
+			}
 		}
 
-		static private void outputSingle(string[] studentRecords)
+		static void FindRecordByIndex()
 		{
-			Console.WriteLine("\nPlease enter the index value");
-			int index;
-			while (true)
+			Console.Write("Enter Index No. :");
+			try
 			{
-				try
+				int index = Convert.ToInt32(Console.ReadLine());
+				WriteRecordToConsole(records[index]);
+			}
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine("Not a valid index");
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("Not a number");
+			}
+		}
+
+		static void FindFirstMatch()
+		{
+			Console.Write("Enter a search string: ");
+			string searchString = Console.ReadLine();
+			var record = FindFirstMatch(searchString);
+			WriteRecordToConsole(record);
+		}
+
+		static string FindFirstMatch(string searchString)
+		{
+			return records.Where(r => r.Contains(searchString)).First();
+		}
+
+		static void ReadFileIntoRecordsArray(string fileName)
+		{
+			using (StreamReader reader = new StreamReader(fileName))
+			{
+				int i = 0;
+				while (!reader.EndOfStream)
 				{
-					index = Convert.ToInt32(Console.ReadLine());
-					break;
-				}
-				catch (FormatException)
-				{
-					Console.WriteLine("Invalid Input");
+					records.Add(reader.ReadLine());
+					i++;
 				}
 			}
-			string studentDetails = studentRecords[index].Replace(",","\t");
-			Console.WriteLine("\n"+studentDetails);
 		}
-		static private void outputAll(string[] studentRecords)
+
+		static void WriteRecordToConsole(string record)
 		{
-			Console.WriteLine();
-			foreach (var details in studentRecords)
+			var formatted = record.Replace(",", "\t");
+			Console.WriteLine(formatted);
+		}
+
+		static void ListedAllRecords()
+		{
+			foreach (var record in records.OrderBy(g => g.Split(',')[4]))
 			{
-				Console.WriteLine(details);
+				WriteRecordToConsole(record);
+			}
+		}
+
+		static void FindRecordByStudentNumber()
+		{
+			Console.Write("Enter Student No. :");
+			int number = Convert.ToInt32(Console.ReadLine());
+			//Get the number from the user
+			var record = records.Where(s => Convert.ToInt32(s.Split(',')[0]) == number).FirstOrDefault();
+			if (record != null)
+			{
+				WriteRecordToConsole(record);
+			}
+			else
+			{
+				Console.WriteLine("Invalid Number");
 			}
 		}
 	}
