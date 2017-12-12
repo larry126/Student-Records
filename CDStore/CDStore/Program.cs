fac469231a0d2c1498af935bc0c83d21f9580094
Larry Zhao <245313315@qq.com>
2017-12-12 11:24:37 +0000

    Slide 6 (Finale)

--- CDStore/CDStore/Program.cs
+++ CDStore/CDStore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CDStore
 {
@@ -11,9 +12,10 @@ namespace CDStore
             bool keepGoing = true;
             while (keepGoing)
             {
-                Console.Write("\n\nEnter 1 to add an Artist \n2 to List Artists \n3 Find artist\n9 to Quit : ");
+                Console.Write("\n\nEnter \n1 Add Artist \n2 to List Artists \n3 Find artist \n4 Add CD \n5 List CDs \n6 Find CD \n7 Add Song \n8 List Songs \n9 Quit : ");
                 char key = Console.ReadLine()[0];
-                switch (key)
+				Console.Write('\n');
+				switch (key)
                 {
                     case '1':
                         AddArtist(context);
@@ -24,14 +26,105 @@ namespace CDStore
                     case '3':
                         FindArtist(context);
                         break;
-                    case '9':
+					case '4':
+						AddCD(context);
+						break;
+					case '5':
+						ListCDs(context);
+						break;
+					case '6':
+						FindCD(context);
+						break;
+					case '7':
+						AddSong(context);
+						break;
+					case '8':
+						ListSongs(context);
+						break;
+					case '9':
                         keepGoing = false;
                         break;
                 }
             }
         }
 
-        private static void FindArtist(CDStoreDbContext context)
+		private static void AddCD(CDStoreDbContext context)
+		{
+			Console.WriteLine("Enter title of new CD: ");
+			var cdTitle = Console.ReadLine();
+			Console.Write("Enter Record Company of new CD: ");
+			string RC = Console.ReadLine();
+			Console.Write("Enter published date of the new CD (YYYY/MM/DD): ");
+			string date = Console.ReadLine();
+			DateTime PD = Convert.ToDateTime(date);
+			List<Song> songs = new List<Song>();
+			while (true)
+			{
+				Console.Write("Enter song title (Q to stop): ");
+				string title = Console.ReadLine();
+				if (title.ToLower() == "q")
+				{
+					break;
+				}
+				else
+				{
+					var song = context.Songs.FirstOrDefault(st => st.Title.Contains(title));
+					songs.Add(song);
+				}
+				//moreSong = title.ToLower() == "q" ? false : true;
+			}
+			CD cd = new CD() { Title = cdTitle, RecordCompany = RC, Published = PD, Songs = songs};
+			Console.WriteLine("Saving ...");
+			context.CD.Add(cd);
+			context.SaveChanges();
+		}
+
+		private static void ListSongs(CDStoreDbContext context)
+		{
+			foreach (Song s in context.Songs)
+			{
+				Console.WriteLine(s.Title + '\t' + s.MusicType + '\t' + s.Artist.Name);
+			}
+			Console.WriteLine();
+		}
+
+		private static void AddSong(CDStoreDbContext context)
+		{
+			Console.WriteLine("Enter Artist's name: ");
+			var name = Console.ReadLine();
+			var artist = context.Artists.FirstOrDefault(a => a.Name.Contains(name));
+			Console.Write("Enter title of new song: ");
+			string title = Console.ReadLine();
+			Console.Write("Enter music type of new song: ");
+			string musicType = Console.ReadLine();
+			Song s = new Song() { Title = title, MusicType = musicType, Artist = artist };
+			Console.WriteLine("Saving ...");
+			context.Songs.Add(s);
+			context.SaveChanges();
+		}
+
+		private static void FindCD(CDStoreDbContext context)
+		{
+			Console.WriteLine("Enter CD's title: ");
+			var Title = Console.ReadLine();
+			var CD = context.CD.FirstOrDefault(a => a.Title.Contains(Title));
+			Console.WriteLine("CD Title: " + CD.Title);
+			foreach (Song s in CD.Songs)
+			{
+				Console.WriteLine(s.Title + '\t' + s.Artist.Name);
+			}
+		}
+
+		private static void ListCDs(CDStoreDbContext context)
+		{
+			foreach (CD c in context.CD)
+			{
+				Console.WriteLine(c.Title + " " + c.Id);
+			}
+			Console.WriteLine();
+		}
+
+		private static void FindArtist(CDStoreDbContext context)
         {
             Console.WriteLine("Enter Artist's name: ");
             var name = Console.ReadLine();
@@ -42,7 +135,6 @@ namespace CDStore
 			{
 				Console.WriteLine(s.Title + '\t' + s.MusicType);
 			}
-
 		}
 
 		private static void ListArtists(CDStoreDbContext context)
